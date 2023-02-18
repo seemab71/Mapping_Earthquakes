c10935d3edef6471126d20364f3c2a7d26e91b8d
Seemab H <111551902+seemab71@users.noreply.github.com>
2023-02-17 18:15:12 -0800

    Add files via upload

---
+++ Static/js/markers.js
@@ -0,0 +1,80 @@
+// Ensure JavaScript is working
+
+console.log("Working");
+
+
+
+
+// Create tile layer
+
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/satellite-streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+// Create maps variable
+let baseMaps = {
+    "Satellite": satelliteStreets,
+    "Streets": streets
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+});
+
+L.control.layers(baseMaps).addTo(map);
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+
+
+    function styleInfo(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: "#ffae42",
+            color: "#000000",
+            radius: getRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+    }
+
+    // The radius of an earthquake marker is 4 times its magnitude
+    // If an earthquake has a magnitude of 0, its marker will have radius 1
+    function getRadius(magnitude) {
+        if (magnitude === 0) {
+            return 1;
+        }
+
+        return magnitude * 4;
+    }
+
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJSON(data, {
+        // turn each feature into a CircleMarker
+        pointToLayer: function (feature, latlng) {
+            console.log(feature);
+            return L.circleMarker(latlng);
+        },
+        // add style
+        style: styleInfo
+    }).addTo(map);
+});
